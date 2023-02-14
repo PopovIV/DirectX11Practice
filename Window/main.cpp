07aec11ecdcf0b7a12b5e884bf764b6dfea5fc52
PopovIV <71888203+PopovIV@users.noreply.github.com>
2023-02-14 05:40:42 +0300

    Added task2

--- Window/main.cpp
+++ Window/main.cpp
@@ -37,7 +37,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         pRenderer->Render();
     }
 
-    pRenderer->CleanupDevice();
+    pRenderer->Cleanup();
 
     return (int)msg.wParam;
 }
--- Window/renderer.h
+++ Window/renderer.h
@@ -1,8 +1,17 @@
 // Renderer.h - class for initializing dx11 and basic rendering
 #pragma once
 
-#include <d3d11.h>
+#include <d3dcompiler.h>
 #include <dxgi.h>
+#include <d3d11.h>
+#include <directxmath.h>
+
+using namespace DirectX;
+
+struct Vertex {
+    float x, y, z;
+    COLORREF color;
+};
 
 class Renderer {
   public:
@@ -10,18 +19,27 @@ class Renderer {
     bool InitDevice(HWND hWnd);
     // Render the frame
     bool Render();
-    // Clean up the objects we've created
-    void CleanupDevice();
+    // Clean up all the objects we've created
+    void Cleanup();
     // Resize window
     bool Resize(UINT width, UINT height);
 
   private:
     HRESULT SetupBackBuffer();
+    // Function to initialize scene's geometry
+    HRESULT InitScene();
 
     ID3D11Device* m_pDevice = nullptr;
     ID3D11DeviceContext* m_pContext = nullptr;
     IDXGISwapChain* m_pSwapChain = nullptr;
     ID3D11RenderTargetView* m_pBackBufferRTV = nullptr;
+
+    ID3D11Buffer* m_pVertexBuffer = nullptr;
+    ID3D11Buffer* m_pIndexBuffer = nullptr;
+    ID3D11InputLayout* m_pInputLayout = nullptr;
+    ID3D11VertexShader* m_pVertexShader = nullptr;
+    ID3D11PixelShader* m_pPixelShader = nullptr;
+
     UINT m_width = 0;
     UINT m_height = 0;
 };
